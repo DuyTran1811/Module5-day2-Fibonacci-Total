1af345ef786b6027a1a58478bf53ba092b39a9e9
DuyTran1811 <ferssken99@gmail.com>
2021-04-29 11:51:16 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-function checFibonacci(num: number):any {
+function checFibonacci(num) {
     if (num <= 2) {
         return 1;
     }
     return checFibonacci(num - 1) + checFibonacci(num - 2);
 }
-let sum = 0;
-for(let i = 2; i < 10; i++){
+var sum = 0;
+for (var i = 2; i < 10; i++) {
     console.log(checFibonacci(i));
-    if (checFibonacci(i)){
+    if (checFibonacci(i)) {
         sum += checFibonacci(i);
     }
 }
-console.log("Tổng Giá Trị Dãy Số Fibonacci "+ sum);
+console.log("Tổng Giá Trị Dãy Số Fibonacci " + sum);
